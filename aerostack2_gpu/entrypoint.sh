31e97c1b64cdc9fc8b866c914b4bec7c83b541dc
GPatiA2 <g.glenza@upm.es>
2025-06-04 12:03:32 +0200

    Added custom .bashrc to automatically source aerostack2 and project

--- aerostack2_gpu/entrypoint.sh
+++ aerostack2_gpu/entrypoint.sh
@@ -14,4 +14,7 @@ cp /root/.gitconfig_local /root/.gitconfig
 #git config --global --add safe.directory /root/aerostack2_ws/src/aerostack2
 git config --global --add safe.directory '*'
 
+. /opt/ros/$ROS_DISTRO/setup.sh && rosdep install --from-paths /root/aerostack2_ws/src --ignore-src -r -y
+# source $AEROSTACK2_PATH/as2_cli/setup_env.bash
+# echo 'source $AEROSTACK2_PATH/as2_cli/setup_env.bash' >> .bashrc
 exec "$@"
